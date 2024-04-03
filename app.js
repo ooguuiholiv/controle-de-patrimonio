6165fd7daa40982e21a8647cde88fa901098744f
Guilherme Franco <francoguilherme.sec@gmail.com>
2024-04-03 14:24:41 -0300

    :construction:  feat:  Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
-require('dotenv').config()
-const connectDB = require('./config/db')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const user_routes = require("./routes/user_routes");
 
 const port = process.env.PORT;
-const app = express()
-connectDB()
 
-app.use(express.json())
-app.use(cors())
-app.use(logger('dev'))
+const app = express();
+connectDB();
 
-app.listen(port, ()=>{
-    console.log(`Server is running in port ${port}`);
-})
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(logger("dev"));
+
+// Routes
+app.use(user_routes);
+
+app.listen(port, () => {
+  console.log(`Server is running in port ${port}`);
+});

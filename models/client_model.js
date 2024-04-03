6e41b048ecd9529154d575a317e6ebc7af4f07a5
Guilherme Franco <francoguilherme.sec@gmail.com>
2024-04-03 14:04:48 -0300

    :construction:  feat:  Create client_model

---
+++ models/client_model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const clientScheme = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  document_id: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: false,
+  },
+  //   Entra como guest automaticamente
+  type_user: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+  constructions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Constructions",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Client", clientScheme)

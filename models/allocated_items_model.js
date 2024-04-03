602bb973787e930380104497341fc6851e0e0250
Guilherme Franco <francoguilherme.sec@gmail.com>
2024-04-03 13:48:27 -0300

    :construction:  feat:  Update in Allocate_items_model

--- models/allocated_items_model.js
+++ models/allocated_items_model.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose')
 
 const AllocatedItemsScheme = new mongoose.Schema({
-    fullname: {
-        type: String,
-        required: true
+  
+  construction_id: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Constructions",
     },
-    items: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Patrimony"
-        }
-    ]
-})
+  ],
+  vehicle_id: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vehicle",
+    },
+  ],
+  patrimony_id: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patrimony"
+    }
+  ]
+});
 // TODO Rever esse items - acredito estar passando informação errada
 module.exports = mongoose.model("AllocatedItems", AllocatedItemsScheme);

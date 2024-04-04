c701652d65e165f9a9c6dcb84b894993befdfa52
Guilherme Franco <francoguilherme.sec@gmail.com>
2024-04-04 13:26:57 -0300

    :sparkles:  feat:  Entering user authentication route

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -6,7 +6,33 @@ const User = require("../models/user_model");
 const Client = require("../models/client_model");
 const router = express.Router();
 
+// Loga o usuário na aplicação
+router.post("/auth/user", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!existingUser || !isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ msg: "Email or password are not in our database" });
+    }
+    const payload = {
+      user: {
+        id: existingUser.id,
+        fullname: existingUser.first_name + existingUser.last_name,
+      },
+    };
+    const bearerToken = await jwt.sign(payload, secretJwt, {
+      expiresIn: "12h",
+    });
+    return res.status(200).json({ msg: "Signed-In successfully", bearerToken });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;

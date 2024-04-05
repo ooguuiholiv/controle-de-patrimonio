98d85562eff715bdac144b8fa7c3bd8bd04605fc
Guilherme Franco <francoguilherme.sec@gmail.com>
2024-04-05 15:31:12 -0300

    :recycle:  refactor:  Correcting nomenclatures and adjusting display format

--- models/teams_model.js
+++ models/teams_model.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose')
 
 const teamsScheme = new mongoose.Schema({
-  teams_id: [
-    {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Employee",
-    },
-  ],
   leader_id: [
     {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Employee"
     }
+  ],
+  teams_members_id: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Employee",
+    },
   ]
+  
 });
 
 module.exports = mongoose.model('Team', teamsScheme)

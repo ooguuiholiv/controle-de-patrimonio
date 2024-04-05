d346ede3fe5e71bae8fa7825a750d757193c0593
Guilherme Franco <francoguilherme.sec@gmail.com>
2024-04-05 16:55:03 -0300

    :sparkles:  feat:  Inserting post type route to register a vehicle. Only users can access this route

--- routes/vehicle_routes.js
+++ routes/vehicle_routes.js
@@ -1,8 +1,55 @@
-const express = require('express')
-const router = express.Router()
-const Vehicle = require('../models/vehicle_model')
-const User = require('../models/user_model')
-const Employee = require('../models/employee_model')
-const isAuthenticated = require("../middlewares/auth")
+const express = require("express");
+const router = express.Router();
+const Vehicle = require("../models/vehicle_model");
+const User = require("../models/user_model");
+const Employee = require("../models/employee_model");
+const isAuthenticated = require("../middlewares/auth");
+const mongoose = require("mongoose");
+const { validatePlate } = require("../utils/validators");
 
-module. exports = router
+router.post("/create/vehicle", isAuthenticated, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ msg: "You do not have permission to access this path" });
+    }
+    const { fleet, plate, conductorId } = req.body;
+    console.log("requisições recebidas", plate);
+    if (!fleet || !plate || !conductorId) {
+      return res
+        .status(400)
+        .json({ msg: "Fleet, plate and conductor is required" });
+    }
+    if (!mongoose.Types.ObjectId.isValid(conductorId)) {
+      return res.status(400).json({ msg: "the driver ID is invalid" });
+    }
+    const conductor = await Employee.findById(conductorId);
+    if (!conductor) {
+      return res.status(400).json({ msg: "Conductor not found" });
+    }
+    if (!validatePlate(plate)) {
+      return res.status(400).json({ err: "Error: Invalid plate" });
+    }
+    if (fleet.length > 4) {
+      return res
+        .status(400)
+        .json({ msg: "The fleet number has only 4 characters" });
+    }
+    
+    const vehicleData = {
+      fleet,
+      plate,
+      conductor,
+      is_active: true,
+    };
+    const newVehicle = new Vehicle(vehicleData);
+    await newVehicle.save();
+    return res.status(201).json({ msg: "Vehicle created succesfully" });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
+  }
+});
+
+module.exports = router;

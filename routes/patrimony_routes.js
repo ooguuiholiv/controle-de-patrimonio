4eb5da206903a0510817ecbf372aa33c61b2ce93
Guilherme Franco <francoguilherme.sec@gmail.com>
2024-04-08 10:01:07 -0300

    :sparkles:  feat:  Adding routes

---
+++ routes/patrimony_routes.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user_model')
+const Patrimony = require('../models/patrimony_model')
+
+module.exports = router
